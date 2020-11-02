b9430d868d1b128dfbcd1883a9db278e223a6af2
viktoriya maslennikova <libikvikaa@yandex.ru>
2020-11-02 19:21:49 +0500

    NewCode

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Стоимость билета
+        double ticketCost = 3600.99;
+        System.out.println((int)(ticketCost / 20));
+    }
+}
